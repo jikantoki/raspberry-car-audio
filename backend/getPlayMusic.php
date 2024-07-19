9a82de2b9301bc15e77a483f38a37387a49f24f9
jikantoki <61790160+jikantoki@users.noreply.github.com>
2024-07-20 02:58:37 +0900

    make getPlayMusic.php

--- backend/getPlayMusic.php
+++ backend/getPlayMusic.php
@@ -1,4 +1,20 @@
 <?php
+
+/** Bluetoothの現在のオーディオ情報を取得 */
 $getSource = passthru('(echo menu player && echo show) | bluetoothctl');
-echo $getSource;
-echo 'あああ';
+$includeEnterWordString = str_replace(array("\r\n", "\r"), "\n", $getSource);
+/** 取得したデータが一行ごとに配列になっている */
+$arraiedString = explode("\n", $includeEnterWordString);
+/** 現在の再生状況 */
+$player = array(
+  'title' => null,
+  'artist' => null,
+  'album' => null
+);
+foreach ($arraiedString as $string) {
+  if (strpos($string, 'No default player available')) {
+    echo 'オワタ';
+    break;
+  }
+}
+echo json_encode($player);
